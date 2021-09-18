de2d8236c889dc02990f60c41d7198a9ea266ead
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-09-18 16:15:08 +0100

    Text area with basic transformations and undo functionality (#2)
    * feat: working example with text box and basic functions
    
    * fix: rename line -> lines
    
    * feat: basic undo functionality
    
    * feat: store actions
    
    * feat: don't log repeated items to memory
    
    * feat: button to clear text box
    
    * feat: panel area
    
    * fix: typo
    
    * feat: linesEqualSelf
    
    * refactor: textProcessor
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-function smokeTest() {
-    console.log("JavaScript loaded.");
-}
+function rowToLines(text) {
+    return text.split(",").join("\n");
+};
+
+
+function linesToRow(text) {
+    return text.split("\n").join(",");
+};
+
+function linesEqualSelf(text) {
+    return text.split("\n").map(x => x + '=' + x).join("\n");
+};
