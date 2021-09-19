4d2c39b256abb0f4270072f90b65c3064f66489a
HaeckelK <haeckelk.github@gmail.com>
2021-09-19 09:55:22 +0100

    feat: get user input if required - prefix example

--- scripts.js
+++ scripts.js
@@ -2,11 +2,14 @@ function rowToLines(text) {
     return text.split(",").join("\n");
 };
 
-
 function linesToRow(text) {
     return text.split("\n").join(",");
 };
 
 function linesEqualSelf(text) {
     return text.split("\n").map(x => x + '=' + x).join("\n");
+};
+
+function linesPrefix(text, prefix='') {
+    return text.split("\n").map(x => prefix + x).join("\n");
 };
