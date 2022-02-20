61b7c123f7b03393506f91b9073d0a28ffad585b
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2022-02-20 15:44:04 +0000

    Argument component (#39)
    * feat: display args in JSON format on page for reference
    
    * feat: add delete button at emit delete message
    
    * feat: add Edit button with no action
    
    * feat: catch delete-argument event
    
    * feat: implement deleteArgument
    
    * feat: demo add argument button
    
    * feat: use ID in argument def
    
    * feat: empty form for add argument
    
    * remove: unneeded title
    
    * feat: buttons for moving up and down, no functionality
    
    * feat: working moveArgumentUp
    
    * feat: working moveArgumentDown
    
    * feat: delete all arguments button
    
    * feat: toggle new argument style vs free text input
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- build/components.js
+++ build/components.js
@@ -1,5 +1,5 @@
 const PageBanner = {
-    template: `<header>
+  template: `<header>
     <div class="navbar navbar-default">
       <div class="navbar-header">
         <h1>{{ pagename }}</h1>
@@ -8,3 +8,53 @@ const PageBanner = {
   </header>`,
   props: ['pagename']
 };
+
+const CLIAgumentDisplay = {
+  template: `<div>
+  <h5> {{ arg }} </h5>
+  <div class="btn-group">
+  <button type="button" class="btn btn-danger" @click="deleteArgument">
+    Delete
+  </button>
+  <button type="button" class="btn btn-info">
+    Edit
+  </button>
+  <button type="button" class="btn btn-secondary" @click="moveUp">
+    <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+  </button>
+  <button type="button" class="btn btn-secondary" @click="moveDown">
+    <span class="glyphicon glyphicon-arrow-down" aria-hidden="false"></span>
+  </button>
+</div>
+</div>`,
+  props: ['arg'],
+  methods: {
+    deleteArgument() {
+      this.$emit("delete-argument");
+    },
+    moveUp() {
+      this.$emit('move-up', this.arg.id);
+    },
+    moveDown() {
+      this.$emit('move-down', this.arg.id);
+    }
+  }
+};
+
+const CLIArgumentForm = {
+  template: `<div>
+  <form @submit.prevent="onSubmit">
+    <button type="submit" class="btn btn-primary">
+      Add
+    </button>
+  </form>
+</div>`,
+methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit('add-argument');
+    }
+  }
+}
